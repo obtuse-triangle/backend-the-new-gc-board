b09a6cc1c29a445547b2fc518973ea7811659ce8
Obtuse triangle <me@obtuse.kr>
2025-12-08 13:31:03 +0900

    fix: generateStaticParams 반환 타입 명시

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import "../globals.css";
 
 export const dynamic = "force-static";
 
-export function generateStaticParams() {
+export function generateStaticParams(): { locale: Locale }[] {
   return locales.map((locale) => ({ locale }));
 }
 
